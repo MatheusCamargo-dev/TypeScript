3fc5caf4e3861a5662a918a45b65cec2ee55f727
matheuscamargo <matheuscamargodev@gmail.com>
2023-03-12 12:23:56 -0300

    type any in typescript

---
+++ src/A0002-type-any/index.ts
@@ -0,0 +1,15 @@
+// Use any only as a last resort
+
+// function showMessage(msg: any) {
+//   return msg;
+// }
+
+// console.log(showMessage([1, 2, 3]));
+// console.log(showMessage('Hello world'));
+// console.log(showMessage(123));
+
+function showString(msg: string) {
+  return msg;
+}
+
+showString('hii');
