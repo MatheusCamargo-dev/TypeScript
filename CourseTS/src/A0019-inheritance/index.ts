0abf62eb3f8af0c2f76eaf287d753c829a618fa3
Matheus Romeiro Camargo <matheuscamargodev@gmail.com>
2023-03-13 17:17:51 -0300

    Class Inheritance in TypeScript

---
+++ CourseTS/src/A0019-inheritance/index.ts
@@ -0,0 +1,37 @@
+export class Person {
+  constructor(
+    public firstName: string,
+    public lastName: string,
+    private age: number,
+    protected cpf: string,
+
+  ){}
+
+
+  public getFullName() : string {
+    return 'Person: '+this.lastName+ ' '+this.firstName;
+  }
+
+
+}
+export class Student extends Person {
+  public getFullName() : string {
+    return 'Student: '+this.lastName+ ' '+this.firstName;
+  }
+};
+
+export class Client extends Person {
+  public getFullName() : string {
+    return 'Client: '+this.firstName+ ' '+this.lastName;
+  }
+};
+
+const person = new Person('Luffy', 'Monkey D.', 21, '111.111.111-11');
+const student = new Student('Zoro', 'Roronoa', 21, '111.111.111-11');
+const client = new Client('Sanji', 'Vinsmoke', 26, '111.111.111-11');
+
+console.log(person.getFullName())
+console.log(student.getFullName())
+console.log(client.getFullName())
+
+
