24433ca59ffd1ed4da6c6744902f50ad11124022
Matheus Romeiro Camargo <matheuscamargodev@gmail.com>
2023-03-14 15:38:20 -0300

    private constructor in TypeScript

---
+++ CourseTS/src/A0024-private-constructor/index.ts
@@ -0,0 +1,27 @@
+// Singleton - GoF
+
+export class Database {
+  private static database: Database;
+  private constructor(
+    private host: string,
+    private user: string,
+    private password: string
+  ){}
+
+  connect(): void{
+    console.log(`Connected: ${this.host}, ${this.user}, ${this.password}`);
+  }
+
+  static getDatabase(
+    host: string,
+    user: string,
+    password: string
+  ){
+    if(Database.database) return Database.database;
+    Database.database = new Database(host, user, password);
+    return Database.database;
+  };
+
+}
+
+const db = Database.getDatabase('localhost', 'root', '123');
