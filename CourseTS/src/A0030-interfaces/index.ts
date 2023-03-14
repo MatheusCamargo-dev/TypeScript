c0dffb4060ac39430a9ebe20f054f72155f0b211
matheuscamargo <matheuscamargodev@gmail.com>
2023-03-14 20:33:15 -0300

    interfaces in typescript

---
+++ CourseTS/src/A0030-interfaces/index.ts
@@ -0,0 +1,15 @@
+interface TypePerson {
+  firstName: string;
+  lastName: string;
+  fullName(): string;
+};
+
+export class Person implements TypePerson {
+  constructor(public firstName: string, public lastName: string) {}
+
+  fullName(): string {
+    return this.firstName + ' ' + this.lastName;
+  }
+}
+const p1 = new Person('Sanji', 'Vinsmoke');
+console.log(p1.fullName());
